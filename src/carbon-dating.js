52d864930b125df55262f5028cb36843b2d9af59
MaxD <max.moderation@gmail.com>
2022-05-02 18:25:11 +0300

    feat: done carbon-dating sol

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,9 +18,16 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(sampleActivity) {
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-  if (typeof sampleActivity !== 'number') {
+  const reactionSpeed = 0.693 / HALF_LIFE_PERIOD;
+  let age;
+
+  if (+sampleActivity <= 0 || +sampleActivity > 15 || isNaN(sampleActivity)) {
+    return false;
+  } else if (typeof sampleActivity == 'string') {
+    age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / reactionSpeed);
+  } else {
     return false;
   }
+
+  return age;
 }
