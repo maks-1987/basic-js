98fc8cfeb4cd20e807448778f076d46f9230cac2
MaxD <max.moderation@gmail.com>
2022-05-02 14:15:42 +0300

    feat: done dreamTeam solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,39 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {NotImplementedError} from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  let dreamTeam = '';
+  let bufferTeam = [];
+  let name = '';
+
+  if (members == null) return false;
+
+  for (let i = 0; i < members.length; i++) {
+    if (parseFloat(members[i])) {
+      continue;
+    } else if (typeof members[i] == 'string') {
+      name = members[i].replace(/\s+/g, '');
+      bufferTeam.push(name[0].toUpperCase());
+    }
+  }
+
+  bufferTeam.sort();
+
+  for (let name of bufferTeam) {
+    dreamTeam += name;
+  }
+
+  return dreamTeam;
 }
